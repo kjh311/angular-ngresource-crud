52e35558e46146d6e5801f7e590f27e5c6f2cca0
Kevin Huelsmann <kjh311@gmail.com>
2016-09-08 08:04:03 -0700

    getting stuck on delete

--- js/myapp.module.js
+++ js/myapp.module.js
@@ -3,16 +3,21 @@
 // http://schemaform.io/  ANGULAR SCHEMA fORM
 // https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 // https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
+// https://coderwall.com/p/gjs7wq/angular-s-resource-module-is-amazing
 
-angular.module('myapp')
-.factory('ProductService', ['$resource',
-    function ($resource) {
-        return $resource('/products', {}, {
-            query: { method: "GET", isArray: true },
-            create: { method: "POST"},
-            get: { method: "GET"},
-            remove: { method: "DELETE"},
-            update: { method: "PUT"}
-        });
+angular.module('myapp', ['ngResource'])
+// .factory('Student', ['$resource',
+//     function ($resource) {
+//         return $resource('/products', {}, {
+//             query: { method: "GET", isArray: true },
+//             create: { method: "POST"},
+//             get: { method: "GET"},
+//             remove: { method: "DELETE"},
+//             update: { method: "PUT"}
+//         });
+
+app.factory('Students', function($resource){
+    return $resource('/localhost:3000/api/student:id');
+});
 
 }]);
